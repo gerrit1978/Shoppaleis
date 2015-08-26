b6ef2dc4dcd4e65dbcccd00b2ba81bec2020ecc8
Gerrit Vos <gerrit@ubuntu.virtual>
2015-08-26 08:09:13 +0200

    Content etc

--- sites/all/themes/shoppaleis/templates/page.tpl.php
+++ sites/all/themes/shoppaleis/templates/page.tpl.php
@@ -55,18 +55,16 @@
       <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php print render($tabs); ?>
       <?php print render($page['help']); ?>
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
+      <?php print render($page['content_top']); ?>
       <?php print render($page['content']); ?>
+      <?php print render($page['content_center']); ?>
+      <?php print render($page['content_bottom']); ?>
       <?php print $feed_icons; ?>
     </div>
 
